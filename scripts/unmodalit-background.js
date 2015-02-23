fa0c18dc479bc0942f0164eec13e53e08f0be738
Gustavo Maloste <gustavomaloste@gmail.com>
2015-02-23 10:13:14 -0300

    Messaging between extension and content script impl.

--- scripts/unmodalit-background.js
+++
@@ -1,8 +0,0 @@
-chrome.tabs.onUpdated.addListener(function(tabId, info, tab){
-    chrome.pageAction.show(tabId);
-});
-
-chrome.pageAction.onClicked.addListener(function(){
-    chrome.tabs.executeScript(null, { file: "scripts/mlst.js" });
-    chrome.tabs.executeScript(null, { file: "scripts/unmodalit.js" });
-});
